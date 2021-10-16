a1617b393c7621a5e6bddbae3b8f79d172f390dd
Salim Mayaleh <salim.mayaleh@gmail.com>
2021-10-16 21:32:03 +0200

    init DI ext

--- Readme.md
+++ Readme.md
@@ -1,15 +1,37 @@
-# Maya.Bulkgate.Sms.Extension.Microsoft.DependencyInjection
+# Maya.BulkGate.Sms.Extension.Microsoft.DependencyInjection
 
-[![NuGet](https://img.shields.io/nuget/v/Maya.Bulkgate.Sms.Extension.Microsoft.DependencyInjection.svg)](https://www.nuget.org/packages/Maya.Bulkgate.Sms.Extension.Microsoft.DependencyInjection)
+[![NuGet](https://img.shields.io/nuget/v/Maya.BulkGate.Sms.Extension.Microsoft.DependencyInjection.svg)](https://www.nuget.org/packages/Maya.BulkGate.Sms.Extension.Microsoft.DependencyInjection)
 
 ## Get it
 
-Install Maya.Bulkgate.Sms.Extension.Microsoft.DependencyInjection from the package manager console:
+Install Maya.BulkGate.Sms.Extension.Microsoft.DependencyInjection from the package manager console:
 
 ```
-PM> Install-Package Maya.Bulkgate.Sms.Extension.Microsoft.DependencyInjection
+PM> Install-Package Maya.BulkGate.Sms.Extension.Microsoft.DependencyInjection
 ```
 
 
 ## Usages
 
+`````c#
+...
+using Maya.BulkGate.Sms.Extension.Microsof.DependencyInjection;
+
+...
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMayaBulkGateSms(Configuration);
+    }
+}
+`````
--- src/Maya.Bulkgate.Sms.Ext.MS.DI/DependencyInjection.cs
+++ src/Maya.Bulkgate.Sms.Ext.MS.DI/DependencyInjection.cs
@@ -1,12 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Maya.BulkGate.Sms.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
-namespace Maya.Bulkgate.Sms.Extension.Microsof.DependencyInjection
+namespace Maya.BulkGate.Sms.Extension.Microsof.DependencyInjection
 {
-    public class DependencyInjection
+    public static class DependencyInjection
     {
+        /// <summary>
+        /// Add Maya.BulkGate.Sms.IClient service
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configureOptions"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMayaBulkGateSms(this IServiceCollection services, Action<IServiceProvider, IConfig> configureOptions)
+        {
+            services.AddOptions<IConfig>()
+                .Configure<IServiceProvider>((options, resolver) => configureOptions(resolver, options))
+                .PostConfigure(options =>
+                {
+                    if (string.IsNullOrWhiteSpace(options.ApplicationId))
+                    {
+                        throw new ArgumentNullException(nameof(options.ApplicationId));
+                    }
+                    if (string.IsNullOrWhiteSpace(options.ApplicationToken))
+                    {
+                        throw new ArgumentNullException(nameof(options.ApplicationToken));
+                    }
+                });
+
+            return services.AddSingleton<IClient>(resolver => new Client(resolver.GetRequiredService<IConfig>()));
+        }
+
+        /// <summary>
+        /// Add Maya.BulkGate.Sms.IClient service
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configureOptions"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMayaBulkGateSms(this IServiceCollection services, Action<IConfig> configureOptions)
+        {
+            return services.AddMayaBulkGateSms((_, options) => configureOptions(options));
+        }
+
+        /// <summary>
+        /// Add Maya.BulkGate.Sms.IClient service
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMayaBulkGateSms(this IServiceCollection services, IConfiguration configuration)
+        {
+            IConfig config = new Config();
+            configuration.GetSection("MayaBulkGateSms").Bind(config);
+
+            if (string.IsNullOrWhiteSpace(config.ApplicationId))
+            {
+                throw new ArgumentNullException(nameof(config.ApplicationId));
+            }
+
+            if (string.IsNullOrWhiteSpace(config.ApplicationToken))
+            {
+                throw new ArgumentNullException(nameof(config.ApplicationToken));
+            }
+
+            return services.AddSingleton<IClient>(_ => new Client(config));
+        }
     }
 }
